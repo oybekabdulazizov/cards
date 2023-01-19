e38782bfaadd3c39459587d2487b0df3448f8874
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-01-19 20:45:28 +0100

    load the data from the url using axios, update deck state, and print a card on button-click, and remove the React.StrictMode

--- src/Deck.js
+++ src/Deck.js
@@ -1,19 +1,69 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 
-const BASE_URL = 'https://deckofcardsapi.com/api/deck/';
+const BASE_URL = 'https://deckofcardsapi.com/api/deck';
 
 class Deck extends Component {
   constructor(props) {
     super(props);
     this.state = {
       deck: null,
-      drawnDecks: [],
+      drawnCards: [],
       dataLoaded: false,
     };
+    this.drawCard = this.drawCard.bind(this);
+  }
+
+  async componentDidMount() {
+    await axios.get(`${BASE_URL}/new/shuffle`).then((res) => {
+      setTimeout(
+        function () {
+          this.setState({ deck: res.data, dataLoaded: true });
+        }.bind(this),
+        1000
+      );
+    });
+  }
+
+  async drawCard() {
+    const cardRes = await axios.get(
+      `${BASE_URL}/${this.state.deck.deck_id}/draw`
+    );
+    console.log(cardRes);
+
+    const card = cardRes.data.cards[0];
+
+    this.setState((currState) => ({
+      deck: { ...currState.deck, remaining: cardRes.data.remaining },
+      drawnCards: [
+        ...currState.drawnCards,
+        {
+          code: card.code,
+          value: card.value,
+          suit: card.suit,
+          image: card.image,
+        },
+      ],
+    }));
+  }
+
+  loadingMessage() {
+    return <h2>Loading...</h2>;
   }
 
   render() {
-    return <div>Deck</div>;
+    return (
+      <div>
+        {this.state.dataLoaded ? (
+          <div>
+            <h2>Card Dealer</h2>
+            <button onClick={this.drawCard}>Draw me a card!</button>
+          </div>
+        ) : (
+          this.loadingMessage()
+        )}
+      </div>
+    );
   }
 }
 
